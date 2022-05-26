08abc70931b533844edb3eb27e38d57c0e42bef5
nardoshood <nardostilahun83@gmail.com>
2022-05-26 19:24:35 +0300

    Unit test added

---
+++ test/test_utillities.py
@@ -0,0 +1,24 @@
+
+import unittest
+import pandas as pd
+import numpy as np
+import sys
+import os
+from pprint import pprint
+import json
+sys.path.append('../')
+from scripts.utilities import Utilities
+
+
+
+class TestUtilities(unittest.TestCase):
+
+
+    def test_get_df(self):
+        utilities = Utilities()
+        train = utilities.get_df(path='data/train.csv',rep='../',rev='tagv1')
+        self.assertEqual(len(train), 1017209)
+    
+    
+if __name__ == '__main__':
+    unittest.main()
