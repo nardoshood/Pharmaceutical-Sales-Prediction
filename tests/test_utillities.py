9cfb28dda7efebb67f79d671ec7b4f9ef0060173
nardoshood <nardostilahun83@gmail.com>
2022-05-26 19:37:52 +0300

    updated test file

--- tests/test_utillities.py
+++ tests/test_utillities.py
@@ -16,7 +16,7 @@ class TestUtilities(unittest.TestCase):
 
     def test_get_df(self):
         utilities = Utilities()
-        train = utilities.get_df(path='data/train.csv',rep='../',rev='tagv1')
+        train = utilities.get_df(path='data/train.csv',rep='./',rev='tagv1')
         self.assertEqual(len(train), 1017209)
     
     
